3fca60bd917f11316c1fef2e4cfc0febcfe3b71a
temaakop <akopov_as@mail.ru>
2024-03-10 22:55:24 +0300

    add vuetify and changes WelcomeView

--- chat-project/src/views/WelcomeView.vue
+++ chat-project/src/views/WelcomeView.vue
@@ -5,54 +5,34 @@ const router = useRouter()
 </script>
 
 <template>
-  <div class="container">
-    <div class="authorization-container">
-      <h1 class="title">CHATNEWS</h1>
-      <div class="button-container">
-        <button @click="router.push('/authorization')" class="authorization-button">
+  <div class="d-flex h-screen align-center justify-center bg-indigo-lighten-3">
+    <v-card
+      class="py-6 d-flex align-center flex-column justify-center rounded bg-indigo"
+      :width="350"
+    >
+      <v-card-title class="text-h3 mb-6">CHATNEWS</v-card-title>
+      <v-card-actions class="d-flex justify-space-between">
+        <v-btn
+          prepend-icon="mdi-account-circle"
+          rounted="xs"
+          elevation="4"
+          @click="router.push('/authorization')"
+          class="bg-indigo-lighten-5"
+        >
           Регистрация
-        </button>
-        <button @click="router.push('/login')" class="login-button">Войти</button>
-      </div>
-    </div>
+        </v-btn>
+        <v-btn
+          rounted="xs"
+          elevation="4"
+          @click="router.push('/login')"
+          class="bg-indigo-lighten-5"
+        >
+          <template v-slot:prepend>
+            <v-icon icon="mdi-login"></v-icon>
+          </template>
+          Войти
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
-
-<style scoped>
-.container {
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(63, 15, 107, 0.568);
-}
-
-.authorization-container {
-  padding: 15px;
-  gap: 10px;
-  background-color: rgba(116, 27, 199, 0.911);
-  border-radius: 5px;
-  border: 1px solid white;
-  width: 500px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.button-container {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 20px;
-}
-
-button {
-  cursor: pointer;
-  height: 25px;
-
-  background-color: rgb(247, 244, 248);
-  border: none;
-  border-radius: 2px;
-}
-</style>
